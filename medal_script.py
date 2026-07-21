3f9f80800ec083e6c9a4e4feb8793b98dd0e5768
Rubenherobr1 <196861340+Rubenherobr1@users.noreply.github.com>
2026-07-21 20:14:36 +0100

    feat: correctly copy named clips and handle edge cases

--- medal_script.py
+++ medal_script.py
@@ -1,6 +1,4 @@
 import sqlite3 as sqlite
-import pyshortcuts as pyshort # https://github.com/newville/pyshortcuts - pip install pyshortcuts
-
 from pathlib import Path
 
 
@@ -47,7 +45,7 @@ def decodeTitle(metadata, titleIDPos):
 
 
 # find db path
-medalPath = Path(Path().home(), "AppData", "Roaming", "Medal") 
+medalPath = Path(Path.home(), "AppData", "Roaming", "Medal")
 
 for path in medalPath.iterdir():
     if path.suffix == ".db":
@@ -60,33 +58,62 @@ for path in medalPath.iterdir():
             break
 
 
-# connect to sqlite database and get the video path and it's metadata
+# connect to sqlite database and get the video id, path and it's metadata
 db = sqlite.connect(dbPath) 
-resultSet = db.execute("SELECT video_path, metadata FROM contents")
+resultSet = db.execute("SELECT remote_content_id, video_path, metadata FROM contents")
 
-print("Connected to database and executed query.")
+print("Connected to database and executed query")
 
 
-namedCount = 0
+# create the folder if it dosen't exist yet
+clipsDir = Path("Named-clips")
+clipsDir.mkdir(exist_ok=True)
 
-for path, metadata in resultSet: 
-    #print(f"{metadata}\n\n")
 
-    # get the title, if it exists
-    titleIDPos = metadata.index(b"title") + len("title") # byte that's after the title key
+MAX_PATH_LEN = 260 # on Windows. Bigger paths can exist on linux
+titleList = [] # used to see how many times a title repeat
+
+for id, path, metadata in resultSet: 
+    path = Path(path)
+
+    # get the title
+    titleIDPos = metadata.index(b"title") + len("title")
     title = decodeTitle(metadata, titleIDPos)
 
     if title is None: continue
+
+    print(f"Found '{title}', copying...")
+    titleList.append(title)
+
+
+    # alter the clip's title when necessary
+    if (nRepeats := titleList.count(title)) > 1: # if the title is repeated
+        title += f"-{nRepeats}"
+
+    if len(str(clipsDir)) + len(title) + len(path.suffix) > MAX_PATH_LEN:
+        charsLeft = MAX_PATH_LEN - (len(str(clipsDir)) + len(path.suffix))
+        title = title[:charsLeft] 
+
+        if not title:
+            raise NotImplementedError("Path is too big to copy a file with a name to it")
+
     
-    print(f"Found a named clip: '{title}'")
-    namedCount+=1
+    # copy clip to the target folder
+    targetPath = clipsDir / (title + path.suffix)
+    path.copy(targetPath) # still runs if the file exists, to prevent possible errors from medal's db data changing
 
 
+print(f"Finished sorting through clips. Copied {len(titleList)} files")
 
 
 '''
 TODO:
-- Save the clips as shortcuts in a folder to occupy almost no space
+- Make a JSON file with a list of the files that i have upon executing the script.
+Compare it with the current JSON file to see which files have been removed, and
+check for it's existance to make sure the folder was created.
+- Add "instalation" and "usage" section to README (?)
+- Warn about deleting or altering the JSON file, and explain how the script works
+on README (summed up)
 - Maybe save the clips to an album on Medal.
 '''
 
